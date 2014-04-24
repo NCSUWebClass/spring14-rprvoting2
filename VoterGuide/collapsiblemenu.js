bdda785ff0016914d2eed5d5a7a161d48728f945
Matt <mbaker4@ncsu.edu>
2014-04-24 09:54:00 -0400

    Modified the candidate accordion menu to only allow one section open at a time.
    There is a bug where the table column divisions would become visible if
    one tab and the tab(s) below it were opened. If you closed the tab above
    the visible column lines, the problem would fix itself. If you opened
    the tabs from the bottom-up, the bug would not show up.

--- VoterGuide/collapsiblemenu.js
+++ VoterGuide/collapsiblemenu.js
@@ -1,18 +1,23 @@
-$(document).ready(
-		function() {
-			$(".candidate_county_commission_item");
-			$(".candidate_county_commission tr:first-child").show();
-			$(".candidate_table tr.candidate_county_commission").click(
-					function() {
-						$(this).nextAll("tr.candidate_county_commission_item")
-								.fadeToggle("fast");
-					});
-		});
+$(document).ready(function() {
+		$(".candidate_county_commission_item").hide();
+		$(".candidate_county_commission tr:first-child").show();
+		$(".candidate_table tr.candidate_county_commission").click( function() {
+			$(this).nextAll("tr.candidate_county_commission_item").fadeToggle("fast");
+			$(".candidate_wake_da_item").hide();
+			$(".candidate_clerk_of_court_item").hide();
+			$(".candidate_nc_senate_15_item").hide();
+			$(".candidate_nc_house_49_item").hide();
+	});
+});
 $(document).ready(function() {
 	$(".candidate_wake_da_item").hide();
 	$(".candidate_wake_da tr:first-child").show();
 	$(".candidate_table tr.candidate_wake_da").click(function() {
 		$(this).nextAll("tr.candidate_wake_da_item").fadeToggle();
+		$(".candidate_county_commission_item").hide();
+		$(".candidate_clerk_of_court_item").hide();
+		$(".candidate_nc_senate_15_item").hide();
+		$(".candidate_nc_house_49_item").hide();
 	});
 });
 $(document).ready(function() {
@@ -20,6 +25,10 @@ $(document).ready(function() {
 	$(".candidate_clerk_of_court tr:first-child").show();
 	$(".candidate_table tr.candidate_clerk_of_court").click(function() {
 		$(this).nextAll("tr.candidate_clerk_of_court_item").fadeToggle();
+		$(".candidate_county_commission_item").hide();
+		$(".candidate_wake_da_item").hide();
+		$(".candidate_nc_senate_15_item").hide();
+		$(".candidate_nc_house_49_item").hide();
 	});
 });
 $(document).ready(function() {
@@ -27,6 +36,10 @@ $(document).ready(function() {
 	$(".candidate_nc_senate_15 tr:first-child").show();
 	$(".candidate_table tr.candidate_nc_senate_15").click(function() {
 		$(this).nextAll("tr.candidate_nc_senate_15_item").fadeToggle();
+		$(".candidate_county_commission_item").hide();
+		$(".candidate_clerk_of_court_item").hide();
+		$(".candidate_wake_da_item").hide();
+		$(".candidate_nc_house_49_item").hide();
 	});
 });
 $(document).ready(function() {
@@ -34,6 +47,10 @@ $(document).ready(function() {
 	$(".candidate_nc_house_49 tr:first-child").show();
 	$(".candidate_table tr.candidate_nc_house_49").click(function() {
 		$(this).nextAll("tr.candidate_nc_house_49_item").fadeToggle();
+		$(".candidate_county_commission_item").hide();
+		$(".candidate_clerk_of_court_item").hide();
+		$(".candidate_nc_senate_15_item").hide();
+		$(".candidate_wake_da_item").hide();
 	});
 });
 
