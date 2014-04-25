1a2f82ae80b6a53d667cccaf519dab36d53d191e
lestocks <lestocks@ncsu.edu>
2014-04-25 00:22:18 -0400

    Added hover functions for all candidates

--- VoterGuide/collapsiblemenu.js
+++ VoterGuide/collapsiblemenu.js
@@ -56,7 +56,200 @@ $(document).ready(function() {
 
 $(document).ready(function() {
 $("#joeBryan").hover(
-	function(e) { $("#hiddendiv_joeBryan").show().css('top', (e.pageY-100)).css('left', (e.pageX+100)); }, 
+	function(e) { $("#hiddendiv_joeBryan").show().css('top', (e.pageY-100)).css('left', (e.pageX)); }, 
+	function() { $("#hiddendiv_joeBryan").hide(); }
+);
+$("#hiddendiv_joeBryan").hover(
+	function(e) { $("#hiddendiv_joeBryan").show().css('top', (e.pageY-100)).css('left', (e.pageX)); }, 
 	function() { $("#hiddendiv_joeBryan").hide(); }
 );
 });
+
+$(document).ready(function() {
+$("#fredPuryear").hover(
+	function(e) { $("#hiddendiv_fredPuryear").show().css('top', (e.pageY-100)).css('left', (e.pageX)); }, 
+	function() { $("#hiddendiv_fredPuryear").hide(); }
+);
+$("#hiddendiv_fredPuryear").hover(
+	function(e) { $("#hiddendiv_fredPuryear").show().css('top', (e.pageY-100)).css('left', (e.pageX)); }, 
+	function() { $("#hiddendiv_fredPuryear").hide(); }
+);
+});
+
+$(document).ready(function() {
+$("#nancyFreeman").hover(
+	function(e) { $("#hiddendiv_nancyFreeman").show().css('top', (e.pageY-100)).css('left', (e.pageX)); }, 
+	function() { $("#hiddendiv_nancyFreeman").hide(); }
+);
+$("#hiddendiv_nancyFreeman").hover(
+	function(e) { $("#hiddendiv_nancyFreeman").show().css('top', (e.pageY-100)).css('left', (e.pageX)); }, 
+	function() { $("#hiddendiv_nancyFreeman").hide(); }
+);
+});
+
+$(document).ready(function() {
+$("#jeffCruden").hover(
+	function(e) { $("#hiddendiv_jeffCruden").show().css('top', (e.pageY-100)).css('left', (e.pageX)); }, 
+	function() { $("#hiddendiv_jeffCruden").hide(); }
+);
+$("#hiddendiv_jeffCruden").hover(
+	function(e) { $("#hiddendiv_jeffCruden").show().css('top', (e.pageY-100)).css('left', (e.pageX)); }, 
+	function() { $("#hiddendiv_jeffCruden").hide(); }
+);
+});
+
+$(document).ready(function() {
+$("#terrySwaim").hover(
+	function(e) { $("#hiddendiv_terrySwaim").show().css('top', (e.pageY-100)).css('left', (e.pageX)); }, 
+	function() { $("#hiddendiv_terrySwaim").hide(); }
+);
+$("#hiddendiv_terrySwaim").hover(
+	function(e) { $("#hiddendiv_terrySwaim").show().css('top', (e.pageY-100)).css('left', (e.pageX)); }, 
+	function() { $("#hiddendiv_terrySwaim").hide(); }
+);
+});
+
+$(document).ready(function() {
+$("#benjaminZellinger").hover(
+	function(e) { $("#hiddendiv_benjaminZellinger").show().css('top', (e.pageY-100)).css('left', (e.pageX)); }, 
+	function() { $("#hiddendiv_benjaminZellinger").hide(); }
+);
+$("#hiddendiv_benjaminZellinger").hover(
+	function(e) { $("#hiddendiv_benjaminZellinger").show().css('top', (e.pageY-100)).css('left', (e.pageX)); }, 
+	function() { $("#hiddendiv_benjaminZellinger").hide(); }
+);
+});
+
+$(document).ready(function() {
+$("#jeffersonGriffin").hover(
+	function(e) { $("#hiddendiv_jeffersonGriffin").show().css('top', (e.pageY-100)).css('left', (e.pageX)); }, 
+	function() { $("#hiddendiv_jeffersonGriffin").hide(); }
+);
+$("#hiddendiv_jeffersonGriffin").hover(
+	function(e) { $("#hiddendiv_jeffersonGriffin").show().css('top', (e.pageY-100)).css('left', (e.pageX)); }, 
+	function() { $("#hiddendiv_jeffersonGriffin").hide(); }
+);
+});
+
+$(document).ready(function() {
+$("#johnBryant").hover(
+	function(e) { $("#hiddendiv_johnBryant").show().css('top', (e.pageY-100)).css('left', (e.pageX)); }, 
+	function() { $("#hiddendiv_johnBryant").hide(); }
+);
+$("#hiddendiv_johnBryant").hover(
+	function(e) { $("#hiddendiv_johnBryant").show().css('top', (e.pageY-100)).css('left', (e.pageX)); }, 
+	function() { $("#hiddendiv_johnBryant").hide(); }
+);
+});
+
+$(document).ready(function() {
+$("#blairWilliams").hover(
+	function(e) { $("#hiddendiv_blairWilliams").show().css('top', (e.pageY-100)).css('left', (e.pageX)); }, 
+	function() { $("#hiddendiv_blairWilliams").hide(); }
+);
+$("#hiddendiv_blairWilliams").hover(
+	function(e) { $("#hiddendiv_blairWilliams").show().css('top', (e.pageY-100)).css('left', (e.pageX)); }, 
+	function() { $("#hiddendiv_blairWilliams").hide(); }
+);
+});
+
+$(document).ready(function() {
+$("#jenniferKnox").hover(
+	function(e) { $("#hiddendiv_jenniferKnox").show().css('top', (e.pageY-100)).css('left', (e.pageX)); }, 
+	function() { $("#hiddendiv_jenniferKnox").hide(); }
+);
+$("#hiddendiv_jenniferKnox").hover(
+	function(e) { $("#hiddendiv_jenniferKnox").show().css('top', (e.pageY-100)).css('left', (e.pageX)); }, 
+	function() { $("#hiddendiv_jenniferKnox").hide(); }
+);
+});
+
+$(document).ready(function() {
+$("#joeTeague").hover(
+	function(e) { $("#hiddendiv_joeTeague").show().css('top', (e.pageY-100)).css('left', (e.pageX)); }, 
+	function() { $("#hiddendiv_joeTeague").hide(); }
+);
+$("#hiddendiv_joeTeague").hover(
+	function(e) { $("#hiddendiv_joeTeague").show().css('top', (e.pageY-100)).css('left', (e.pageX)); }, 
+	function() { $("#hiddendiv_joeTeague").hide(); }
+);
+});
+
+$(document).ready(function() {
+$("#samBridges").hover(
+	function(e) { $("#hiddendiv_samBridges").show().css('top', (e.pageY-100)).css('left', (e.pageX)); }, 
+	function() { $("#hiddendiv_samBridges").hide(); }
+);
+$("#hiddendiv_samBridges").hover(
+	function(e) { $("#hiddendiv_samBridges").show().css('top', (e.pageY-100)).css('left', (e.pageX)); }, 
+	function() { $("#hiddendiv_samBridges").hide(); }
+);
+});
+
+$(document).ready(function() {
+$("#barbaraMoore").hover(
+	function(e) { $("#hiddendiv_barbaraMoore").show().css('top', (e.pageY-100)).css('left', (e.pageX)); }, 
+	function() { $("#hiddendiv_barbaraMoore").hide(); }
+);
+$("#hiddendiv_barbaraMoore").hover(
+	function(e) { $("#hiddendiv_barbaraMoore").show().css('top', (e.pageY-100)).css('left', (e.pageX)); }, 
+	function() { $("#hiddendiv_barbaraMoore").hide(); }
+);
+});
+
+$(document).ready(function() {
+$("#aprylMajor").hover(
+	function(e) { $("#hiddendiv_aprylMajor").show().css('top', (e.pageY-100)).css('left', (e.pageX)); }, 
+	function() { $("#hiddendiv_aprylMajor").hide(); }
+);
+$("#hiddendiv_aprylMajor").hover(
+	function(e) { $("#hiddendiv_aprylMajor").show().css('top', (e.pageY-100)).css('left', (e.pageX)); }, 
+	function() { $("#hiddendiv_aprylMajor").hide(); }
+);
+});
+
+$(document).ready(function() {
+$("#jimFulghum").hover(
+	function(e) { $("#hiddendiv_jimFulghum").show().css('top', (e.pageY-100)).css('left', (e.pageX)); }, 
+	function() { $("#hiddendiv_jimFulghum").hide(); }
+);
+$("#hiddendiv_jimFulghum").hover(
+	function(e) { $("#hiddendiv_jimFulghum").show().css('top', (e.pageY-100)).css('left', (e.pageX)); }, 
+	function() { $("#hiddendiv_jimFulghum").hide(); }
+);
+});
+
+$(document).ready(function() {
+$("#kimHanchette").hover(
+	function(e) { $("#hiddendiv_kimHanchette").show().css('top', (e.pageY-100)).css('left', (e.pageX)); }, 
+	function() { $("#hiddendiv_kimHanchette").hide(); }
+);
+$("#hiddendiv_kimHanchette").hover(
+	function(e) { $("#hiddendiv_kimHanchette").show().css('top', (e.pageY-100)).css('left', (e.pageX)); }, 
+	function() { $("#hiddendiv_kimHanchette").hide(); }
+);
+});
+
+$(document).ready(function() {
+$("#derekKiszely").hover(
+	function(e) { $("#hiddendiv_derekKiszely").show().css('top', (e.pageY-100)).css('left', (e.pageX)); }, 
+	function() { $("#hiddendiv_derekKiszely").hide(); }
+);
+$("#hiddendiv_derekKiszely").hover(
+	function(e) { $("#hiddendiv_derekKiszely").show().css('top', (e.pageY-100)).css('left', (e.pageX)); }, 
+	function() { $("#hiddendiv_derekKiszely").hide(); }
+);
+});
+
+function loadCandidate(name) {
+	var xmlHttp = null;
+	response.addHeader("Access-Control-Allow-Origin", "*");
+	var url = "http://152.1.13.65/view.php?candidate=" + name;
+	xmlHttp = new XMLHttpRequest();
+	xmlHttp.open( "GET", url, false);
+	xmlHttp.send(null);
+	var html = xmlHttp.responseText;
+	alert(html);
+
+}
+
